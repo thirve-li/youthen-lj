90ef436217be53e25d12c2d74496c6c07a2db99f
LiXin <70917176@qq.com>
2017-10-18 17:52:42 +0800

    注册用户列表

--- src/main/java/com/youthen/lj/action/LjAction.java
+++ src/main/java/com/youthen/lj/action/LjAction.java
@@ -5,6 +5,7 @@
 
 package com.youthen.lj.action;
 
+import java.util.List;
 import org.apache.struts2.convention.annotation.Action;
 import org.apache.struts2.convention.annotation.Namespace;
 import org.apache.struts2.convention.annotation.Result;
@@ -15,6 +16,7 @@ import com.youthen.framework.presentation.action.BaseAction;
 import com.youthen.lj.service.LjRoomInfoService;
 import com.youthen.master.persistence.entity.LoginUser;
 import com.youthen.master.service.LoginUserService;
+import com.youthen.master.service.dto.LoginUserDto;
 
 /**
  * 。
@@ -29,18 +31,23 @@ import com.youthen.master.service.LoginUserService;
         @Result(name = "index", location = "/WEB-INF/jsp/lj/admin/index.jsp"),
         @Result(name = "list", location = "/WEB-INF/jsp/lj/admin/notice/list.jsp"),
         @Result(name = "entityInit", location = "/WEB-INF/jsp/lj/admin/notice/entity.jsp"),
-        @Result(name = "view", location = "/WEB-INF/jsp/lj/admin/notice/view.jsp")
+        @Result(name = "view", location = "/WEB-INF/jsp/lj/admin/notice/view.jsp"),
+        @Result(name = "listUser", location = "/WEB-INF/jsp/lj/admin/user/list.jsp"),
+        @Result(name = "listAdmin", location = "/WEB-INF/jsp/lj/admin/user/listAdmin.jsp")
 })
 @Controller
 public class LjAction extends BaseAction {
 
     private LoginUser loginUser;
+    private LoginUserDto userDto;
 
     @Autowired
     private LoginUserService loginUserService;
 
     @Autowired
     private LjRoomInfoService roomInfoService;
+
+    List<LoginUserDto> userList;
 
     /*
      * 主页显示
@@ -49,6 +56,16 @@ public class LjAction extends BaseAction {
     public String index() {
         // this.roomInfoService.createRoomInfo();
         return "index";
+
+    }
+
+    @Action("listUser")
+    public String listUser() {
+        this.userDto = new LoginUserDto();
+        this.userDto.setCompanyId(1l);
+        this.userDto.setDepartmentId(1l);
+        this.userList = this.loginUserService.getUserList(this.userDto);
+        return "listUser";
 
     }
 
@@ -89,6 +106,24 @@ public class LjAction extends BaseAction {
      */
     public void setLoginUser(final LoginUser aLoginUser) {
         this.loginUser = aLoginUser;
+    }
+
+    /**
+     * getter for userList.
+     * 
+     * @return userList
+     */
+    public List<LoginUserDto> getUserList() {
+        return this.userList;
+    }
+
+    /**
+     * setter for userList.
+     * 
+     * @param aUserList userList
+     */
+    public void setUserList(final List<LoginUserDto> aUserList) {
+        this.userList = aUserList;
     }
 
 }
